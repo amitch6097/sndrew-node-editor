98f9e129c0ec62db04ee3c1abdd2af76afa6df1b
amitch6097 <mitcan@Andrew-Mitchells-MacBook-Pro.local>
2020-01-29 20:08:47 -0500

    Updates

---
+++ precache-manifest.885b28a094cc769514fee00a816c290c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7782aeaf0bda5083fd31b63f696c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c129b32f275691e6813",
+    "url": "/static/css/main.a0b33bfa.chunk.css"
+  },
+  {
+    "revision": "5bc64a7c6c4bcb538cc0",
+    "url": "/static/js/2.e3b08594.chunk.js"
+  },
+  {
+    "revision": "8c129b32f275691e6813",
+    "url": "/static/js/main.9f5aa99f.chunk.js"
+  },
+  {
+    "revision": "84d6721e64a18fe343e6",
+    "url": "/static/js/runtime-main.f47b6839.js"
+  }
+]);
