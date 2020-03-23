14b0144a9588dc689c5e0e69c8737e2c376614b1
amitch6097 <mitch702@msu.edu>
2020-03-22 21:58:03 -0400

    Add ability to have a controlled component
    this lets the user control the zoom or position from state above the editor incase the user want to add controls for zooming

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -6,22 +6,26 @@ import Tree from '../Tree/Tree';
 import withDrag from '../../hoc/withDrag';
 import * as EditorHelpers from '../../lib/Editor.helpers';
 
+
 interface IEditorProps {
-    currentPosition?: {
-        x: number;
-        y: number;
-    };
-    onDragContainerMouseDown?: (e: any) => void;
-    Node: React.ComponentClass<any>;
-    nodes: Record<string, SndrewEditor.INode>;
-    headNodeIds: string[];
-    childNodes: Record<string, string[]>;
-    onPathClicked?: (link: SndrewEditor.ILink) => void;
+  currentPosition?: SndrewEditor.IPoint;
+
+  zoom?: number;
+  controlled?: boolean;
+  onZoom?: (nextZoom: number) => void;
+  onPan?: (nextPosition: SndrewEditor.IPoint) => void;
+
+  onDragContainerMouseDown?: (e: any) => void;
+  Node: React.ComponentClass<any>;
+  nodes: Record<string, SndrewEditor.INode>;
+  headNodeIds: string[];
+  childNodes: Record<string, string[]>;
+  onPathClicked?: (link: SndrewEditor.ILink) => void;
 }
 
 interface IEditorState {
-    zoom: number;
-    links: SndrewEditor.ILink[];
+  zoom: number;
+  links: SndrewEditor.ILink[];
 }
 
 const MIN_ZOOM = 0.01;
@@ -29,94 +33,112 @@ const MAX_ZOOM = 3;
 const EDITOR_CONTAINER = 'editor-container-id';
 
 class Editor extends React.Component<IEditorProps, IEditorState> {
-    state = {
-        links: [],
-        zoom: 1,
-    };
+  state = {
+    links: [],
+    zoom: 1,
+  };
 
-    componentDidMount() {
-        window.addEventListener('resize', this.onWindowResize);
-        this.resetPaths();
+  componentDidMount() {
+    window.addEventListener('resize', this.onWindowResize);
+    this.resetPaths();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onWindowResize);
+  }
+
+  componentDidUpdate(prevProps: IEditorProps) {
+    if (this.props.nodes !== prevProps.nodes) {
+      this.resetPaths();
     }
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onWindowResize);
+  onWindowResize = () => {
+    this.resetPaths();
+  };
+
+  resetPaths = () => {
+    const { nodes, childNodes, headNodeIds } = this.props;
+    const zoom = this.getZoom();
+    const links = EditorHelpers.getLinks({
+      ids: headNodeIds,
+      childNodes,
+      nodes,
+      zoom,
+      container: EDITOR_CONTAINER,
+    });
+    this.setState({
+      links: links,
+    });
+  };
+
+  onZoom = (value: number) => {
+    this.setZoom(value);
+  };
+
+
+  setZoom = (zoom: number) => {
+    const lastZoom = this.getZoom();
+    const nextZoom = lastZoom + zoom;
+    const nextZoomSafe = nextZoom < MIN_ZOOM ? MIN_ZOOM : nextZoom > MAX_ZOOM ? MAX_ZOOM : nextZoom
+
+    if (this.props.controlled && this.props.onZoom) {
+      this.props.onZoom(nextZoomSafe)
+    } else {
+      this.setState({
+        zoom: nextZoomSafe
+      });
     }
+  }
 
-    componentDidUpdate(prevProps: IEditorProps) {
-        if (this.props.nodes !== prevProps.nodes) {
-            this.resetPaths();
-        }
-    }
+  getZoom = () => {
+    const { zoom: zoomState } = this.state;
+    const { controlled, zoom: zoomProp } = this.props;
+    const zoom = (controlled && zoomProp) || zoomState;
+    return zoom < MIN_ZOOM ? MIN_ZOOM : zoom > MAX_ZOOM ? MAX_ZOOM : zoom;
+  }
 
-    onWindowResize = () => {
-        this.resetPaths();
-    };
+  render() {
+    const { links } = this.state;
+    const {
+      currentPosition = { x: 0, y: 0 },
+      onDragContainerMouseDown,
+      Node,
+      onPathClicked,
+      headNodeIds,
+    } = this.props;
+    const { x, y } = currentPosition;
 
-    resetPaths = () => {
-        const { zoom } = this.state;
-        const { nodes, childNodes, headNodeIds } = this.props;
-        const links = EditorHelpers.getLinks({
-            ids: headNodeIds,
-            childNodes,
-            nodes,
-            zoom,
-            container: EDITOR_CONTAINER,
-        });
-        this.setState({
-            links: links,
-        });
-    };
-
-    onZoom = (value: number) => {
-        const { zoom } = this.state;
-        const nextZoom = zoom + value;
-        this.setState({
-            zoom: nextZoom < MIN_ZOOM ? MIN_ZOOM : nextZoom > MAX_ZOOM ? MAX_ZOOM : nextZoom,
-        });
-    };
-
-    render() {
-        const { zoom, links } = this.state;
-        const {
-            currentPosition = { x: 0, y: 0 },
-            onDragContainerMouseDown,
-            Node,
-            onPathClicked,
-            headNodeIds,
-        } = this.props;
-        const { x, y } = currentPosition;
-
-        return (
-            <div
-                id="editor"
-                className="editor"
-                onMouseDown={onDragContainerMouseDown}
-                onWheel={e => {
-                    // const x = e.deltaX;
-                    const y = e.deltaY / 1000;
-                    this.onZoom(y);
-                }}
-            >
-                <SVGLayer
-                    style={{
-                        transform: `translate(${x}px, ${y}px) scale(${zoom})`,
-                    }}
-                    onPathClicked={onPathClicked}
-                    links={links}
-                />
-                <div
-                    id={EDITOR_CONTAINER}
-                    className="editor__tree"
-                    style={{
-                        transform: `translate(${x}px, ${y}px) scale(${zoom})`,
-                    }}
-                >
-                    <Tree {...this.props} ids={headNodeIds} Node={Node} />
-                </div>
-            </div>
-        );
-    }
+    const zoom = this.getZoom();
+    return (
+      <div
+        id="editor"
+        className="editor"
+        onMouseDown={onDragContainerMouseDown}
+        onWheel={e => {
+          const y = e.deltaY / 1000;
+          this.onZoom(y);
+        }}
+      >
+        <SVGLayer
+          style={{
+            transform: `translate(${x}px, ${y}px) scale(${zoom})`,
+          }}
+          onPathClicked={onPathClicked}
+          links={links}
+        />
+        <div
+          id={EDITOR_CONTAINER}
+          className="editor__tree"
+          style={{
+            transform: `translate(${x}px, ${y}px) scale(${zoom})`,
+          }}
+        >
+          <Tree {...this.props} ids={headNodeIds} Node={Node} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withDrag(Editor);
--- src/hoc/withDrag.tsx
+++ src/hoc/withDrag.tsx
@@ -1,98 +1,117 @@
 import React from 'react';
 
 export interface IEditContainerState {
-    currentPosition: {
-        x: number;
-        y: number;
-    };
-    lastDragPosition:
-        | {
-              x: number;
-              y: number;
-          }
-        | undefined;
+  controlled?: boolean;
+  onChangePosition?: (nextPosition: SndrewEditor.IPoint) => void;
+
+  currentPosition: {
+    x: number;
+    y: number;
+  };
+
+  lastDragPosition:
+  | {
+    x: number;
+    y: number;
+  }
+  | undefined;
 }
 
 export default function withDrag(WrappedComponent: any) {
-    return class WithDragComponent extends React.Component<any, IEditContainerState> {
-        state: IEditContainerState = {
-            currentPosition: this.props.currentPosition || { x: 0, y: 0 },
-            lastDragPosition: undefined,
-        };
-
-        onTranslate = (xValue: number, yValue: number) => {
-            const { currentPosition } = this.state;
-            this.setState({
-                currentPosition: {
-                    x: currentPosition.x + xValue,
-                    y: currentPosition.y + yValue,
-                },
-            });
-        };
-
-        onMouseDown = (e: any) => {
-            if (e.nativeEvent.which === 1) {
-                const { screenX, screenY } = e;
-                this.setState({
-                    lastDragPosition: {
-                        x: screenX,
-                        y: screenY,
-                    },
-                });
-            }
-        };
-
-        onMouseMove = (e: MouseEvent | any) => {
-            const { lastDragPosition, currentPosition } = this.state;
-            const { screenX, screenY } = e;
-            if (lastDragPosition) {
-                const deltaX = screenX - lastDragPosition.x;
-                const deltaY = screenY - lastDragPosition.y;
-                this.setState({
-                    currentPosition: {
-                        x: currentPosition.x + deltaX,
-                        y: currentPosition.y + deltaY,
-                    },
-                    lastDragPosition: { x: screenX, y: screenY },
-                });
-            }
-        };
-
-        onMouseUp = () => {
-            this.setState({
-                lastDragPosition: undefined,
-            });
-        };
-
-        componentDidUpdate(_prevProps: any, prevState: IEditContainerState) {
-            if (this.state.lastDragPosition && !prevState.lastDragPosition) {
-                //mouse
-                document.addEventListener('mousemove', this.onMouseMove);
-                document.addEventListener('mouseup', this.onMouseUp);
-                //touch
-                document.addEventListener('touchmove', this.onMouseMove);
-                document.addEventListener('touchend', this.onMouseUp);
-                document.addEventListener('touchcancel', this.onMouseUp);
-            } else if (!this.state.lastDragPosition && prevState.lastDragPosition) {
-                //mouse
-                document.removeEventListener('mousemove', this.onMouseMove);
-                document.removeEventListener('mouseup', this.onMouseUp);
-                //touch
-                document.removeEventListener('touchmove', this.onMouseMove);
-                document.removeEventListener('touchend', this.onMouseUp);
-                document.removeEventListener('touchcancel', this.onMouseUp);
-            }
-        }
-
-        render() {
-            return (
-                <WrappedComponent
-                    currentPosition={this.state.currentPosition}
-                    {...this.props}
-                    onTranslate={this.onTranslate}
-                    onDragContainerMouseDown={this.onMouseDown}
-                />
-            );
-        }
+  return class WithDragComponent extends React.Component<any, IEditContainerState> {
+    state: IEditContainerState = {
+      currentPosition: this.props.currentPosition || { x: 0, y: 0 },
+      lastDragPosition: undefined,
     };
+
+    getPosition = () => {
+      const { currentPosition: currentPositionState } = this.state;
+      const { currentPosition: currentPositionProp, controlled } = this.props;
+      return (controlled && currentPositionProp) || currentPositionState;
+    }
+
+    setPosition = (nextPosition: SndrewEditor.IPoint) => {
+      const { onChangePosition, controlled } = this.props;
+      if (controlled && onChangePosition) {
+        onChangePosition(nextPosition);
+      } else {
+        this.setState({
+          currentPosition: nextPosition
+        });
+      }
+    }
+
+    onTranslate = (xValue: number, yValue: number) => {
+      const currentPosition = this.getPosition();
+      this.setPosition({
+        x: currentPosition.x + xValue,
+        y: currentPosition.y + yValue,
+      });
+    };
+
+    onMouseDown = (e: any) => {
+      if (e.nativeEvent.which === 1) {
+        const { screenX, screenY } = e;
+        this.setState({
+          lastDragPosition: {
+            x: screenX,
+            y: screenY,
+          },
+        });
+      }
+    };
+
+    onMouseMove = (e: MouseEvent | any) => {
+      const currentPosition = this.getPosition();
+      const { lastDragPosition } = this.state;
+      const { screenX, screenY } = e;
+      if (lastDragPosition) {
+        const deltaX = screenX - lastDragPosition.x;
+        const deltaY = screenY - lastDragPosition.y;
+        this.setPosition({
+          x: currentPosition.x + deltaX,
+          y: currentPosition.y + deltaY,
+        });
+        this.setState({
+          lastDragPosition: { x: screenX, y: screenY },
+        });
+      }
+    };
+
+    onMouseUp = () => {
+      this.setState({
+        lastDragPosition: undefined,
+      });
+    };
+
+    componentDidUpdate(_prevProps: any, prevState: IEditContainerState) {
+      if (this.state.lastDragPosition && !prevState.lastDragPosition) {
+        //mouse
+        document.addEventListener('mousemove', this.onMouseMove);
+        document.addEventListener('mouseup', this.onMouseUp);
+        //touch
+        document.addEventListener('touchmove', this.onMouseMove);
+        document.addEventListener('touchend', this.onMouseUp);
+        document.addEventListener('touchcancel', this.onMouseUp);
+      } else if (!this.state.lastDragPosition && prevState.lastDragPosition) {
+        //mouse
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('mouseup', this.onMouseUp);
+        //touch
+        document.removeEventListener('touchmove', this.onMouseMove);
+        document.removeEventListener('touchend', this.onMouseUp);
+        document.removeEventListener('touchcancel', this.onMouseUp);
+      }
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          currentPosition={this.getPosition()}
+          onDragContainerMouseDown={this.onMouseDown}
+        />
+      );
+    }
+  };
 }
